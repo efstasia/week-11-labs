7694cba830749e07f243a058177db3fe4c186928
Sofia W <83775090+efstasia@users.noreply.github.com>
2021-10-31 14:00:40 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Components/Header";
+
+export const App = () => {
+  return (
+    <div>
+      <Header title="Get ready for adventure!" />
+    </div>
+  );
+};
